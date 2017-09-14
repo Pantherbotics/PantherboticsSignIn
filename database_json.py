1f952ffab80da7a18d35a7228f608bc03759c795
rogerfachini <r3fachini@live.com>
2017-09-14 12:04:55 -0700

    Working implementation of session parsing

--- database_json.py
+++ database_json.py
@@ -3,21 +3,25 @@
 #   database-json.py: Stores log-in data as JSON files
 # Originally written by Roger Fachini
 
-import os, json, logging, datetime, pprint
+import os, json, logging, time,  datetime, pprint, uuid
 DATA_DIR = "data/"
 STUDENT_DIR = DATA_DIR + "students/"
 EVENT_DIR = DATA_DIR + "eventlog/"
+SESSION_DIR = DATA_DIR + "sessions/"
 
 class Database:
     EVENTS = []
     STUDENTS = {}
+    SESSIONS = []
     def __init__(self):
         self.logger = logging.getLogger('db-json')
         self._initDir(DATA_DIR)
         self._initDir(STUDENT_DIR)
         self._initDir(EVENT_DIR)
+        self._initDir(SESSION_DIR)
         self.loadAllStudents()
         self.loadEventLog()
+        self.loadSessions()
 
     def listStudents(self):
         return [int(str(x).split('.')[0]) for x in os.listdir(STUDENT_DIR) if '.json' in x]
@@ -36,13 +40,15 @@ class Database:
         eventFile = eventTime.strftime("%Y-%m-%d-eventlog.json")
         eventPath = EVENT_DIR + eventFile
         self._initJSON(eventPath, data = [])
-
+        uid = str(uuid.uuid4())
         event = {'id': studentID, 
-                 'timestamp': eventTime.isoformat()}
+                 'timestamp': eventTime,
+                 'uuid': uid}
         if not eventStatus == None:
             event.update({'status':eventStatus})
-
-        self.EVENTS.append(event)
+        
+        self.EVENTS.append(event.copy())
+        event.update({'timestamp': event['timestamp'].isoformat()})
 
         with open(eventPath, 'r') as fHandle:
             data = json.load(fHandle)
@@ -65,6 +71,18 @@ class Database:
                         event['timestamp'] = datetime.datetime.strptime(event['timestamp'], "%Y-%m-%dT%H:%M:%S.%f")
                     self.EVENTS.append(event)
 
+    def loadSessions(self):
+        files = os.listdir(SESSION_DIR)
+        for f in files:
+            with open(SESSION_DIR + f, 'r') as fHandle:
+                data = json.load(fHandle)
+                for session in data:
+                    if 'start_time' in session.keys():
+                        session['start_time'] = datetime.datetime.strptime(session['start_time'], "%Y-%m-%dT%H:%M:%S.%f")
+                    if 'end_time' in session.keys():
+                        session['end_time'] = datetime.datetime.strptime(session['end_time'], "%Y-%m-%dT%H:%M:%S.%f")
+                    self.SESSIONS.append(session)
+
     def loadAllStudents(self):
         for s in self.listStudents():
             fName = "%s%s.json"%(STUDENT_DIR, s)
@@ -84,9 +102,37 @@ class Database:
                 json.dump(data, fHandle, indent=4)
                 fHandle.close()
 
+    def createSession(self, studentID, startTime, endTime, status, startUUID, endUUID):
+        session = {'id': studentID,
+                   'start_time': startTime,
+                   'end_time': endTime,
+                   'duration': (endTime - startTime).total_seconds(),
+                   'status': status,
+                   'start_uuid': startUUID,
+                   'end_uuid': endUUID}
+        sessionFile = startTime.strftime(SESSION_DIR + "%Y-%m-%d-sessions.json")
+        self._initJSON(sessionFile, data = [])
+
+        self.SESSIONS.append(session.copy())
+        session.update({'start_time': session['start_time'].isoformat()})
+        session.update({'end_time': session['end_time'].isoformat()})
+
+        with open(sessionFile, 'r') as fHandle:
+            data = json.load(fHandle)
+            fHandle.close()
+        data.append(session)
+        with open(sessionFile, 'w') as fHandle:
+            json.dump(data, fHandle, indent=4)
+            fHandle.close()
+
 if __name__ == '__main__':
      d = Database()
-     pprint.pprint(d.STUDENTS)
-     pprint.pprint(d.EVENTS)
+     #pprint.pprint(d.STUDENTS)
+     #pprint.pprint(d.EVENTS)
 
-     print d.getEventsFor(128872)
+     #print d.getEventsFor(128872)
+     #start = datetime.datetime.now()
+     #time.sleep(2)
+     #end = datetime.datetime.now()
+     #d.createSession(123456, start, end, 'checkout', 'a', 'b')
+     #pprint.pprint(d.SESSIONS)
--- sessions.py
+++ sessions.py
@@ -9,10 +9,13 @@ import logging, datetime, pprint, time
 # If > 1, sign-in / sign-out times will be rounded to this increment in minutes
 TIME_INCREMENTS = datetime.timedelta(minutes=5)
 
-# Time to begin accepting new sign-ins
-SESSION_START_TIME = datetime.time(hour=11, minute=30, second=0)
+#If a session is longer than this time, it is assumed that the person forgot to sign out
+MAX_SESSION_LENGTH = 57600
 
-SESSION_END_TIME = datetime.time(hour=17, minute=0, second=0)
+#If a session length ends up exceeding MAX_SESSION_LENGTH, their session will be closed at this tome (hour)
+DEF_SESSION_END_HOUR = 17
+DEF_SESSION_END_MINUTE = 0
+DEF_SESSION_END_SECOND = 0
 
 #--------Database Selection--------
 from database_json import Database
@@ -20,9 +23,11 @@ from database_json import Database
 
 class SessionTracker:
     STATE = {}
+    SESSIONS = {}
     def __init__(self):
         self.logger = logging.getLogger('session')
         self.db = Database()
+        self.generateSessions()
         
 
     def studentScanEvent(self, studentID, eventTime = None):
@@ -37,16 +42,21 @@ class SessionTracker:
             self.rescanStudents()
 
         if studentID in self.STATE.keys():
-            currentState = self.STATE[studentID]['state']
             currentTime = self.STATE[studentID]['timestamp']
-            newState = not currentState
-            self.db.appendEventLog(studentID, eventTime, eventStatus = newState)
+            if currentTime == None: currentTime = datetime.datetime.fromtimestamp(0)
+            if self.roundTime(currentTime, TIME_INCREMENTS) == reTime:
+                self.logger.debug('Duplicate scan ignored %s %s %s', studentID, currentTime.isoformat(), eventTime.isoformat())
+                return studentPresent
+
+        if studentID in self.STATE.keys():      
+            currentState = self.STATE[studentID]['state']  
+            newState = not currentState    
+            self.db.appendEventLog(studentID, eventTime)
             self.STATE[studentID].update({'timestamp': eventTime, 
                                           'state': newState})
-            if newState:
-                self.logger.info('Student %s signed in at %s', studentID, eventTime.isoformat())
-            else:
-                self.logger.info('Student %s signed out at %s', studentID, eventTime.isoformat())
+            self.logger.info('Scan event %s %s', studentID, eventTime.isoformat())
+            self.generateSessions()
+
 
         #pprint.pprint(self.STATE)        
         return studentPresent
@@ -60,17 +70,102 @@ class SessionTracker:
         rounding = (seconds+roundTo/2) // roundTo * roundTo
         return dt + datetime.timedelta(0,rounding-seconds,-dt.microsecond)
 
+    def dateFromISO(self, datestr):
+        return datetime.datetime.strptime( datestr, "%Y-%m-%dT%H:%M:%S.%f" )
+
     def rescanStudents(self):
         for studentID in self.db.listStudents():
+            log = self.db.getEventsFor(studentID)
+            if len(log) == 0:
+                tStamp = None
+            else:
+                tStamp = log[-1]['timestamp']
+
             data = {int(studentID):{'state': False,
-                               'timestamp': None}}
+                                    'timestamp': tStamp}}
             self.STATE.update(data)
+    
+    def isStudentSignedIn(self, studentID, dateRefrence=None, length=False):
+        if dateRefrence == None: dateRefrence = datetime.datetime.now()
+        log = self.db.getEventsFor(studentID)
+        if len(log) == 0: return False
+        if len(log) % 2 == 0: return False
+        for event in log:
+            eventLength = dateRefrence - event['timestamp']
+            if eventLength.total_seconds() > 24*60*60:       #Do not process events > 24h old
+                continue
+            else:
+                if length: return (True, eventLength)
+                else: return True
+
+    def generateSessions(self):
+        STATE = {}
+        uuidGroups = [[s['start_uuid'], s['end_uuid']] for s in self.db.SESSIONS]
+        parsedUUIDs =  [uuid for sublist in uuidGroups for uuid in sublist]
+        
+        for event in self.db.EVENTS:
+            eventUUID = event['uuid']
+            if eventUUID in parsedUUIDs: continue
+
+            registerTimeout = False
+            stID = event['id']
+            timestamp = event['timestamp']
+            sessionLength = 0
+            if stID in STATE.keys():
+                sessStatus = STATE[stID]['status']
+                sessUUID = STATE[stID]['uuid']
+                sessTimestamp = STATE[stID]['timestamp']
+                sessionLength = timestamp - sessTimestamp
+                if sessStatus == 'scanin' and sessionLength.total_seconds() >= MAX_SESSION_LENGTH:
+                    backtimestamp = sessTimestamp.replace(hour=DEF_SESSION_END_HOUR, minute=DEF_SESSION_END_MINUTE, second=DEF_SESSION_END_SECOND)
+                    registerTimeout = True
+                    self.db.createSession(stID, sessTimestamp, backtimestamp, 'timeout', sessUUID, None)
+                    STATE.update({stID:{'status': 'timeout', 'timestamp': backtimestamp}})
+
+                if sessStatus == 'scanin' and sessionLength.total_seconds() <= MAX_SESSION_LENGTH:
+                    self.db.createSession(stID, sessTimestamp, timestamp, 'scanout', sessUUID, eventUUID)
+                    newStatus = 'scanout'
+                elif sessStatus == 'scanout' or sessStatus == 'timeout':
+                    newStatus = 'scanin'
+            else:
+                newStatus = 'scanin'
+                sessionLength = 0
+
+            if registerTimeout:
+                self.logger.debug('Student %-18s (%s) Forgot to sign out. length: %s', self.db.STUDENTS[stID]['name'], stID, backtimestamp - sessTimestamp)
+            if newStatus == 'scanin': 
+                self.logger.debug('Student %-18s (%s) Scanned In  at %s length: %s', self.db.STUDENTS[stID]['name'], stID, timestamp.isoformat(), sessionLength)
+            elif newStatus == 'scanout': 
+                self.logger.debug('Student %-18s (%s) Scanned Out at %s length: %s', self.db.STUDENTS[stID]['name'], stID, timestamp.isoformat(), sessionLength)
+                
+
+            STATE.update({stID:{'status': newStatus, 'timestamp': timestamp, 'uuid': eventUUID}})
+
+
+    def isCurrentTimeInSession(self):
+        now = datetime.datetime.now()
+        return now.hour >= SESSION_START_HOUR and now.hour <= SESSION_END_HOUR
+
 
 def consoleTrack():
     s = SessionTracker()
     while True:
-        a = input('#>')
-        a = int(a)
+        st = s.db.listStudents()
+        for i in st:
+            da = s.isStudentSignedIn(i, length=True)
+            if  da == False:
+                print "%-8s %-20s Absent" % (i, s.db.STUDENTS[i]['name'])
+            else:
+                print "%-8s %-20s %s" % (i, s.db.STUDENTS[i]['name'], da[1])
+        try:
+            a = raw_input('#>')
+            a = int(a)
+        except KeyboardInterrupt:
+            exit(0)
+        except ValueError:
+            print "Error: Invalid Input (cannot convert to int)"
+            continue
+        
         suc = s.studentScanEvent(a)
         if not suc:
             name = raw_input("ID not in database; enter student name: ")
@@ -81,9 +176,5 @@ def consoleTrack():
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
 
+    print '  Pantherbotics Electronic Sign-in Sheet'
     consoleTrack()
-
-    s = SessionTracker()
-    print s.studentScanEvent(123456)
-    time.sleep(1)
-    print s.studentScanEvent(123456)
